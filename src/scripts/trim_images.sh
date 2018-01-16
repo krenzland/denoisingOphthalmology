25723bf20d40549f134025b2fe4764804975b27c
Lukas Krenz <lukas@krenz.land>
2018-01-16 22:58:34 +0100

    Add resize to trim_images script.

--- src/scripts/trim_images.sh
+++ src/scripts/trim_images.sh
@@ -9,16 +9,17 @@ fi
 
 raw_dir="$1"
 target_dir="$2"
+extension='tif'
 
 echo "$1"
 find ${raw_dir} \( -name "*.jpeg" -or -name "*.ppm" \) -type f | while read image; do
     raw_path="$(readlink -f $image)"
-    image_name="$(basename ${raw_path})"
-    target_name="$(readlink -f ${target_dir}/${image_name})"
+    image_name="$(basename ${raw_path} | cut -f 1 -d '.')" # Remove extension.
+    target_name="$(readlink -f ${target_dir}/${image_name}.${extension})"
     # TODO: Find better value for fuzz
-    convert "${raw_path}" -fuzz 20% -bordercolor black -trim +repage "${target_name}"
+    convert "${raw_path}" -fuzz 20% -bordercolor black -trim +repage -resize 1024x1024 "${target_name}"
 done
 
 # Fuzz 20% is a bit too eager for very dark images
 # Discard them, they aren't that great anyway.
-find "$target_dir" -size -100 -delete 
+find "$target_dir" type f -size -100k -delete 
