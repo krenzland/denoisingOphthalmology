Add normalization and fix perceptual loss.
Maybe only add norm for perceptual loss later?

src/dataset.py
@@ -8,7 +8,7 @@ from pathlib import Path
 import numpy as np
 from PIL import Image, ImageFilter, ImageFile
 import torch.utils.data as data
-from torchvision.transforms import Compose, RandomCrop, ToTensor, Resize, RandomHorizontalFlip, RandomVerticalFlip, Lambda, CenterCrop, Grayscale, ColorJitter
+from torchvision.transforms import Compose, RandomCrop, ToTensor, Resize, RandomHorizontalFlip, RandomVerticalFlip, Lambda, CenterCrop, Grayscale, ColorJitter, Normalize
 import torchvision.transforms.functional as F
 
 # See: https://github.com/keras-team/keras/issues/5475
@@ -175,6 +175,9 @@ class Dataset(data.Dataset):
         
         self.hr_transform = hr_transform
         self.lr_transforms = lr_transforms
+        self.to_tensor = ToTensor()
+        self.normalize = Normalize(mean=[0.485, 0.456, 0.406],
+                                   std=[0.229, 0.224, 0.225])
         self.verbose = verbose
         
         image_suffixes = ['jpg', 'jpeg', 'tif', 'ppm', 'png']        
@@ -211,8 +214,7 @@ class Dataset(data.Dataset):
         for transform in self.lr_transforms:
             imgs.append(transform(imgs[-1]))
         
-        to_tensor = ToTensor()
-        tensors = [to_tensor(i) for i in reversed(imgs)]
+        tensors = [self.normalize(self.to_tensor(i)) for i in reversed(imgs)]
         # lr, hr2, hr4, hr8 = ...
         # shape for crops of size 100: 12, 25, 50, 100
         return tensors
@@ -276,14 +278,11 @@ def get_lr_transform(crop_size, factor, random=True):
 class HrTransform(object):
     def __init__(self, crop_size, random=True):
         self.crop_size = crop_size
-        # Later, for perceptual loss needed!
-        #self.normalize = Normalize(mean=[0.485, 0.456, 0.406],
-        #                          std=[0.229, 0.224, 0.225])
         if random:
             self.random_scaling = RandomScaling(crop_size)
             self.random_rotation = RandomRotation()
             self.random_flip = RandomFlip()
-            self.color_jitter = ColorJitter(brightness=0.5, contrast=0.5, saturation=0.5, hue=0.25)
+            #self.color_jitter = ColorJitter(brightness=0.5, contrast=0.5, saturation=0.5, hue=0.25)
             self.crop = SmartRandomCrop(crop_size)
         else:
             self.crop = CenterCrop(crop_size)
@@ -296,7 +295,7 @@ class HrTransform(object):
             img = self.crop(img, vessels)
             img = self.random_rotation(img, None)
             img = self.random_flip(img, None)
-            img = self.color_jitter(img)
+            #img = self.color_jitter(img)
             return img
         else:
             return self.crop(img) 

src/loss.py
@@ -4,6 +4,7 @@ from collections import namedtuple
 import torch
 import torch.nn as nn
 from torchvision.models import vgg16, VGG
+from torchvision.transforms import Normalize
 
 class CharbonnierLoss(nn.Module):
     def __init__(self, eps=1e-6):
@@ -36,6 +37,10 @@ class LossNetwork(torch.nn.Module):
         # Drop all layers that are not needed from the model.
         last_layer = max([int(i) for i in self.layer_map])
         self.vgg_layers = nn.Sequential(*list(vgg.features)[:last_layer+1])
+
+        # Deactive gradient computation
+        for param in self.parameters():
+            param.requires_grad = False
         
     def forward(self, x):
         output = {}
@@ -50,17 +55,14 @@ class PerceptualLoss(nn.Module):
         super(PerceptualLoss, self).__init__()
         self.loss_network = loss_network
         self.criterion = criterion
-        
+ 
     def forward(self, x, y):
-        # x/y only have the y-channel, vgg expects RGB values.
-        # Just use y as r/g/b each.
         featX = self.loss_network(x)
         featY = self.loss_network(y)
         
         content_loss = 0.0
         for a, b in zip(featX, featY):
-            # normalize all activations to 1
-            content_loss += self.criterion(a/a.norm(), b/b.norm())
+            content_loss += self.criterion(a, b)
             
         return content_loss
 

src/main.py
@@ -127,7 +127,7 @@ def main():
     optimizer = optim.SGD(model.parameters(), weight_decay=1e-4, lr=args.lr, momentum=0.9)
     optimizer = optim.Adam(model.parameters(), weight_decay=1e-4, lr=args.lr)
     criterion = CharbonnierLoss().cuda()
-    #criterion = make_vgg16_loss(criterion).cuda()
+    criterion = make_vgg16_loss(nn.MSELoss().cuda()).cuda()
 
     if args.checkpoint:
         checkpoint = torch.load(args.checkpoint)