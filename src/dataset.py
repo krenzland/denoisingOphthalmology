Fix scaling transformation.
Avoid change of image ratio now.
This is only a theoretical problem for the used images and scaling
factors but makes script more robust to changes.

src/dataset.py
@@ -38,8 +38,10 @@ class RandomScaling(object):
         s = self.get_params()
         oW, oH = img.size
         # Make sure we are not smaller than the cropsize!
-        nW = max(self.crop_size, int(s * oW))
-        nH = max(self.crop_size, int(s * oH))
+        s = max(s, max(self.crop_size/oW, self.crop_size/oH))
+        
+        nW = int(s * oW)
+        nH = int(s * oH)
         new_size = nW, nH
 
         img = F.resize(img, new_size, self.interpolation)