515fde1723ae105964268e6bd3038282ca8f730f
Lukas Krenz <lukas@krenz.land>
2018-04-09 18:13:35 +0200

    Change const. padding to reflect pad for validation.
    More reasonable choice.

--- src/validate.py
+++ src/validate.py
@@ -7,7 +7,7 @@ import pandas as pd
 import torch
 from torch.autograd import Variable
 from torchvision.transforms import ToTensor, ToPILImage, Normalize, CenterCrop
-from PIL import Image, ImageFilter
+from PIL import Image, ImageFilter, ImageOps
 import PIL.ImageChops as chop
 from skimage.filters import frangi, threshold_otsu, sobel
 from skimage import measure
@@ -121,9 +121,51 @@ def vessels(img, mask):
     vessels = Image.fromarray(vessels*255.0)
     return vessels
 
+def pad(img, new_size=None):
+    #print(img.size)
+    img = np.array(img).transpose(2,0,1) # Channel first
+    size = np.array(img.shape)[1:]
+    if new_size is None:
+        new_size = np.ceil(size/4) * 4
+    diff = new_size - size
+    border = np.zeros(4, dtype=np.int)
+    border[0] = diff[0]//2
+    border[1] = diff[1]//2
+    border[2] = diff[0] - border[0]
+    border[3] = diff[1] - border[1]
+
+    new_img = np.zeros((3, int(new_size[0]),
+                        int(new_size[1])))
+    for i,channel in enumerate(img):
+        channel_pad = np.pad(channel,
+                     ((border[0], border[2]), 
+                      (border[1], border[3])),
+                     'reflect')
+        new_img[i,:,:] = channel_pad
+    new_img = np.uint8(new_img).transpose(1,2,0)
+
+    new_img = Image.fromarray(new_img)
+    return new_img, border
+
+def unpad(img, border=np.array([0,0,0,0]), cut_off_stripe=4):
+    img = np.array(img)
+    new_size = np.array(img.shape[0:-1])
+    # Remove additional stripe of 4 px
+    border = border + cut_off_stripe
+    if len(img.shape) == 3:
+        img = img[border[0]:img.shape[0]-border[2],
+                border[1]:img.shape[1]-border[3]:,
+                :]
+    else:
+        img = img[border[0]:img.shape[0]-border[2],
+                border[1]:img.shape[1]-border[3]]
+ 
+    img = Image.fromarray(img)
+    return img
+
 def main():
     checkpoint_dir = Path('../best_checkpoints/')
-    models = ['l1_noSmartCrop', 'l1', 'perc_256', 'perceptual', 'perceptual_pool4', 'gan', 'wgan', 'saliency']
+    models = ['gan_notfinal', 'saly_perc', 'saliency', 'perceptual']
     model_to_checkpoint = lambda m: checkpoint_dir / '{}.pt'.format(m) 
 
     rows = []
@@ -133,33 +175,30 @@ def main():
         model = LapSRN(depth=10).cuda().eval()
         model.load_state_dict(checkpoint['model_state'])
 
-        drive_dir = Path('../data/raw/DRIVE/training/') 
-        drive_range = range(21,41)
+        drive_dir = Path('../data/raw/DRIVE/test/') 
+        drive_range = range(1,20+1)
 
         for num in drive_range:
+            num = str(num).zfill(2)
             print("Analysing image {} for model {}.".format(num, model_name))
             # Load one drive image with mask and ground truth segmentation.
-            mask = Image.open(drive_dir / 'mask/{}_training_mask.gif'.format(num))
-            img = Image.open(drive_dir / 'images/{}_training.tif'.format(num))
+            mask = Image.open(drive_dir / 'mask/{}_test_mask.gif'.format(num))
+            img = Image.open(drive_dir / 'images/{}_test.tif'.format(num))
             gt = Image.open(drive_dir / '1st_manual/{}_manual1.gif'.format(num)).convert('YCbCr').split()[0]
 
             to_pil = ToPILImage()
 
             # Find next largest crop size that is divisible by four.
-            crop_size = [int(np.ceil(s/4)*4) for s in img.size]
-            lr_crop_size = [s//4 for s in (crop_size)]
+            hr4_gt, border = pad(img)
 
-            center_crop = CenterCrop(crop_size[::-1]) # pads s.t. img is div. by 4
-            back_crop = CenterCrop(img.size[::-1]) # crop back to orig. img. size
-
-            hr4_gt = center_crop(img)
+            lr_crop_size = [s//4 for s in hr4_gt.size]
             lr = hr4_gt.resize(lr_crop_size, Image.BICUBIC)
 
             elapsed_time, *sr_out = upsample_tensor(model, lr, to_normalize=True, return_time=True)
-            hr2_sr, hr4_sr = [back_crop(to_pil(out)) for out in sr_out]
-            hr2_bic, hr4_bic = [back_crop(out) for out in upsample_bic(lr)]
+            hr2_sr, hr4_sr = [unpad(to_pil(out), border) for out in sr_out]
+            hr2_bic, hr4_bic = [unpad(out, border) for out in upsample_bic(lr)]
 
-            hr4_gt = img
+            hr4_gt = unpad(hr4_gt, border)
             psnr_sr = measure.compare_psnr(np.array(hr4_gt), np.array(hr4_sr))
             psnr_bic = measure.compare_psnr(np.array(hr4_gt), np.array(hr4_bic))
 
@@ -171,7 +210,7 @@ def main():
             frangi_hr, frangi_sr, frangi_bic = [vessels(i, mask=mask).convert('YCbCr').split()[0] for i in [hr4_gt, hr4_sr, hr4_bic]]
 
             frangi_acc_sr, frangi_acc_bic = [acc(frangi_hr, other) for other in [frangi_sr, frangi_bic]]
-            segmentation_acc_hr, segmentation_acc_sr, segmentation_acc_bic =  [acc(gt, other) for other in [frangi_hr, frangi_sr, frangi_bic]]
+            segmentation_acc_hr, segmentation_acc_sr, segmentation_acc_bic =  [acc(unpad(gt), other) for other in [frangi_hr, frangi_sr, frangi_bic]]
 
 
             # TODO: Maybe add accuracy for completely black image as well, or use better measure!
